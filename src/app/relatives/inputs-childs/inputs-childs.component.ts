eb593c6957135d0e36aac061b82dcf3ce69522ab
Rafael Lopes <rafaellopesdonascimento2607@gmail.com>
2024-05-28 23:56:55 -0300

    input de valores usando o metodo @input v-1.01

--- src/app/relatives/inputs-childs/inputs-childs.component.ts
+++ src/app/relatives/inputs-childs/inputs-childs.component.ts
@@ -5,14 +5,22 @@ import { Component, Input, signal } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './inputs-childs.component.html',
-  styleUrl: './inputs-childs.component.css'
+  styleUrl: './inputs-childs.component.css',
 })
 export class InputsChildsComponent {
-  public inputToSet =  signal("Teste signal")
+  public inputToSet = signal('Teste signal');
 
   @Input({
-    required:true
-  }) set valueTest(value: string) {
-    this.inputToSet.set(value)
+    required: true,
+  })
+  set valueTest(value: string) {
+    this.inputToSet.set(value);
   }
+
+  //  segundo input forma simples
+  //@input simple form
+  @Input({
+    required: true,
+  })
+  secondInput: string = '';
 }
--- src/app/relatives/parents/parents.component.ts
+++ src/app/relatives/parents/parents.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 import { InputsChildsComponent } from '../inputs-childs/inputs-childs.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parents',
   standalone: true,
-  imports: [InputsChildsComponent],
+  imports: [InputsChildsComponent, FormsModule],
   templateUrl: './parents.component.html',
-  styleUrl: './parents.component.css'
+  styleUrl: './parents.component.css',
 })
 export class ParentsComponent {
-  public list = <any>[]
+  public list = <any>[];
 
   public inputValue: string = '';
+  // segundo input forma simples
+  public secondValue: string = '';
 
   handleInputChange(event: Event) {
     this.inputValue = (event.target as HTMLInputElement).value;
   }
-
   logValue() {
     console.log(this.inputValue);
-    this.list.push(this.inputValue)
+    this.list.push({ obj1: this.inputValue, obj2: this.secondValue });
   }
 }
